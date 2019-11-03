7ff3ffc5fa0bd766ae6c014ffe68191e6c84f120
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-11-03 15:50:38 +0500

    removing uuid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
-import uuid from "uuid";
 import "./App.css";
 
 class App extends React.Component {
   state = {
-    monsters: [
-      {
-        name: "Frankenstein",
-        id: uuid()
-      },
-      {
-        name: "Zoombie",
-        id: uuid()
-      },
-      {
-        name: "Dracula",
-        id: uuid()
-      }
-    ]
+    monsters: []
   };
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users =>
+        this.setState({
+          monsters: users
+        })
+      );
+  }
   renderMoster = monster => <h1 key={monster.id}>{monster.name}</h1>;
   render() {
     return <div>{this.state.monsters.map(this.renderMoster)}</div>;
